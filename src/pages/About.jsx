226ac491ad57670cf5096df309735f4281ea1224
Jannis Betschki <jannis@Janniss-MacBook-Pro.local>
2020-10-09 11:54:58 +0200

    test commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+    return(
+        <div>
+            <h1>Heading</h1>
+            <p>Paragraph</p>
+        </div>
+    )
+}
+
+export default About;
