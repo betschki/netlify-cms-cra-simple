33878d3233d4fb8b5220ec8a3c45f43a1e7f3019
Jannis Betschki <jannis@Janniss-MacBook-Pro.local>
2020-10-09 11:57:18 +0200

    formatting

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import About from "./pages/About"
 
 function App() {
-    return ( <
-        Router >
-        <
-        Switch >
-        <
-        Route exact path = "/about"
-        component = { About }
-        /> < /
-        Switch > <
-        /Router>
+    return (
+        <Router >
+            <Switch >
+                <Route exact path="/about"
+                    component={About}
+                />
+            </Switch >
+        </Router>
     )
 }
 
